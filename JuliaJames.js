930d437c5da58c957d7340749ee2d8b45e33b7c5
denven <myregs@foxmail.com>
2019-09-25 11:50:22 -0700

    revision of JuliaJames

--- JuliaJames.js
+++ JuliaJames.js
@@ -1,19 +1,14 @@
 var x = 1;
 
 while (x <= 20) {
-	if (x % 3 === 0) {
-		console.log("Julia");
-	}
-
-	if (x % 5 === 0) {
-		console.log("James");
-	}
 
 	if (x % 15 === 0) {
 		console.log("JuliaJames");
-	}
-
-	if (x % 3 !== 0 && x % 5 !== 0) {
+	} else if (x % 3 === 0) {
+		console.log("Julia");
+	} else if (x % 5 === 0) {
+		console.log("James");
+	} else {
 		console.log(x);
 	}
 
