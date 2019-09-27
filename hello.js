1381a4359d337a4bb1969d710b4048e528455e02
denven <myregs@foxmail.com>
2019-09-27 11:59:21 -0700

    hello.js

--- hello.js
+++ hello.js
@@ -5,3 +5,16 @@ const sayHello = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+const sayHelloConsole = function (name) {
+    console.log("Hello, " + name);
+};
+
+sayHelloConsole("John") ;
+
+const returnSayHello = function (name) {
+    return "Hello, " + name;
+};
+
+const greeting = returnSayHello ("John");
+console.log(greeting);
