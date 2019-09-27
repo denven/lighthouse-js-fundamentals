cebb42f595970d5604eda4bfdaf13c5232463565
denven <myregs@foxmail.com>
2019-09-27 13:54:31 -0700

    Age or Hundreds calculator

---
+++ howManyHundreds.js
@@ -0,0 +1,13 @@
+function howManyHundreds(num) {
+  if ( num > 0 ) { 
+    return (num - num % 100) / 100 ;
+   } else {
+    return 0;
+ }       
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
