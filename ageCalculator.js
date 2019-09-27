6ec4b1f82744f3ea27e20e7b79caf5872c16488f
denven <myregs@foxmail.com>
2019-09-27 13:24:54 -0700

    use function to output the age

---
+++ ageCalculator.js
@@ -0,0 +1,7 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+    return name + " is " + (currentYear - yearOfBirth) + " years old.";
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
+console.log(ageCalculator("Susan", 1977, 2019));
