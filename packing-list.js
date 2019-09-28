553e4ad7711dc47cbdbaac0183ea25e38922e623
denven <myregs@foxmail.com>
2019-09-28 10:12:57 -0700

    packling-list print

---
+++ packing-list.js
@@ -0,0 +1,15 @@
+const packingList = ["tent", "bug spray", "sleeping bag"];
+
+// use for loop
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+// use while loop
+console.log("Kitechen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+    console.log(packingList[i]);
+    i++;
+}
